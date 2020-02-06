672b7380b4f8791f98e0610ea76d40e32a832359
Kirill <lebedevkrll@gmail.com>
2020-02-06 15:17:17 +0300

    Yuy

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
+'''Yuy.py code'''
+
 from copy import deepcopy
 from PIL import Image
-tatras = Image.open("durka.jpg")
-(width, height) = tatras.size
-for x in range(0, width):
-	for y in range( 0, height):
-		r = tatras.getpixel((x, y))
-		e = deepcopy(r)
-		s = (e[0] + e[2] + e[1] - 560) * 2
-		tatras.putpixel((x, y), (s, s, s))
-tatras.save('top.png')
+TATRAS = Image.open("durka.jpg")
+(WIDTH, HEIGHT) = TATRAS.size
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        r = TATRAS.getpixel((x, y))
+        e = deepcopy(r)
+        s = (e[0] + e[2] + e[1] - 560) * 2
+        TATRAS.putpixel((x, y), (s, s, s))
+TATRAS.save('top.png')
